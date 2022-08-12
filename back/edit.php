791d6eaaa484d45e6821dd9060938f8474192472
unknown <tsubasababy1013@gmail.com>
2022-08-12 12:58:05 +0800

    更改作品集的頁面做分頁

--- back/edit.php
+++ back/edit.php
@@ -15,7 +15,13 @@ include_once "./base.php";?>
       <td width="10%">刪除</td>
     </tr>
     <?php
-    $rows=$Repo->all();
+    $all=$Repo->math('count',"id");
+    // 每頁五筆資料
+    $div=5;
+    $pages=ceil($all/$div);//算頁數
+                    $now=$_GET['p']??1;//取得當前頁面
+                    $start=($now-1)*$div;//從哪裡開始計算
+                    $rows=$Repo->all(" limit $start,$div");
     foreach($rows as $row){
     ?>
     <tr>
@@ -31,6 +37,24 @@ include_once "./base.php";?>
     </tbody>
   </table>
   <div style="text-align:center">
+            <?php
+                    if(($now-1) > 0){//如果有上一頁的話 就顯示<的按鈕
+                        $p=$now-1;
+                        echo "<a href='?do=edit&p=$p' style='text-decoration:none;'> < </a>";
+                    }
+                    for($i=1;$i<=$pages;$i++){// 印頁碼
+                        $fontsize=($now==$i)?'1.5rem':'';//當前頁數字體放大
+                        echo "<a href='?do=edit&p=$i' style='text-decoration:none;font-size:$fontsize' > ";//?代表當前頁面
+                        echo $i;
+                        echo " </a>";
+                    }
+                    if(($now+1) <= $pages){//如果有下一頁的話 就顯示>的按鈕
+                        $p=$now+1;
+                        echo "<a href='?do=edit&p=$p' style='text-decoration:none;'> > </a>";
+                    }
+                ?>
+  <div style="text-align:center">
+                    <br>
     <input type="hidden" name="table" value="repository">
     <input type="submit" value="確定更改" style="margin-left:3rem">
     <input type="reset" value="重置" style="margin-left:3rem">
